d50a1fe60af4c96a33ffe82307f1026836e84fee
Marvintschi <80694942+Marvintschi@users.noreply.github.com>
2022-01-19 11:06:14 +0100

    Update Fahrzeuge.cs

--- Fahrzeuge.cs
+++ Fahrzeuge.cs
@@ -1,5 +1,60 @@
 class Fahrzeuge{
 
-
+  private string kennzeichen;
+  private double kilometerstand;
+  private int kapazität;
+  private double grundpreis;
+  private double kilometerpreis;
+  
+  public String getKennzeichen(){
+    return kennzeichen;
+  }
+  
+  public void setKennzeichen(string value){
+    kennzeichen = value;
+  }
+  
+  public double getKilometerstand(){
+    return kilometerstand;
+  }
+  
+  public void setKilometerstand(double value){
+    kilometerstand = value;
+  }
+  
+  public int getKapazität(){
+    return kapazität;
+  }
+  
+  public void setKapazität(int value){
+    kapazität = value;
+  }
+  
+  public double getGrundpreis(){
+    return grundpreis;
+  }
+  
+  public void setGrundpreis(double value){
+    grundpreis = value;
+  }
+  
+  public double getKilometerpreis(){
+    return kilometerpreis;
+  }
+  
+  public void setKilometerpreis(double value){
+    kilometerpreis = value;
+  }
+  
+  
+  public Fahrzeug(){
+    kennzeichen = "PIZ-000";
+    kilometerstand = 0.0;
+    kapazität = 15;
+    grundpreis = 2.0;
+    kilometerpreis = 0.40;
+  }
+  
+  
 
 }
